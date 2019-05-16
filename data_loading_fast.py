1d66488691b53175f453a9d9a873cab67e4918ad
austinader <aderaust@msu.edu>
2019-05-15 21:36:31 -0400

    fixing more nusanse

--- data_loading_fast.py
+++ data_loading_fast.py
@@ -568,6 +568,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(external_stylesheets= external_stylesheets)
 
+server = app.server
+
 
 
 app.config['suppress_callback_exceptions']=True
